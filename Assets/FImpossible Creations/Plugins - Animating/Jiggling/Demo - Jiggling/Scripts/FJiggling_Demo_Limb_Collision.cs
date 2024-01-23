8726f4ffa7662b91c2d7e4b2035161c5b79d7f07
Callum Palmer-Walsh <palmerwalshc@gmail.com>
2024-01-23 18:35:53 +0000

    Jiggle Package + edits

---
+++ Assets/FImpossible Creations/Plugins - Animating/Jiggling/Demo - Jiggling/Scripts/FJiggling_Demo_Limb_Collision.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace FIMSpace.Jiggling
+{
+    public class FJiggling_Demo_Limb_Collision : FJiggling_Simple
+    {
+        public bool NoRotationKeyframes = false;
+        private Quaternion initialKeyRotation;
+
+        protected override void Init()
+        {
+            if (initialized) return;
+
+            base.Init();
+            initialKeyRotation = TransformToAnimate.localRotation;
+        }
+
+        protected override void Update() { }
+
+        protected virtual void LateUpdate()
+        {
+            if (NoRotationKeyframes) transform.localRotation = initialKeyRotation;
+
+            // Every beginning of late update rotations are the same as in animation played by Animator component
+            initRotation = transform.localRotation;
+
+            base.Update();
+            //if (animationFinished) enabled = true;
+        }
+
+        // Use OnCollisionEnter or OnTriggerEnter based on your needs
+        private void OnCollisionEnter(Collision collision)
+        {
+            // Check if the collider that hits the object has a specific tag or properties
+            if (collision.collider is CapsuleCollider)
+            {
+                StartJiggle();
+            }
+        }
+    }
+}
